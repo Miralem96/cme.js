03079dccc0d88e033b8f93ac4b0d61d47769a771
Miralem Idriz <merreidriz@gmail.com>
2022-05-13 15:46:56 +0200

    ticket

--- event.js
+++ event.js
@@ -1,19 +1,97 @@
 class Event {
     static loadEvent() {
+        
+        // Hamburger target selectors
         const hamburger = document.querySelector(".hamburger");
         const navList = document.querySelector(".nav-list");
         const navRemove = document.querySelectorAll(".nav-list");
+
+        // Buy ticket selectors
+        const ticketForm = document.querySelector(".ticket-form");
+        const input = document.querySelectorAll(".input");
+        const name = document.querySelector(".name")
+        const email = document.querySelector(".email");
+        const ticket = document.querySelector("#ticket");
+        const popupCard = document.querySelector(".popup-card");
         
 
+        let ticketNumber = Math.floor(Math.random() * 5000);
+        
+        ticketForm.addEventListener("submit", form);
+        
+        // Toggle the hamburger navigation for mobile
         hamburger.addEventListener("click", function(event){
             hamburger.classList.toggle("active");
             navList.classList.toggle("active");
         })
-
+        // Remove the hamburger navigation for mobile
         navRemove.forEach(element => element.addEventListener("click", () => {
             hamburger.classList.remove("active");
             navList.classList.remove("active");
         }))
+
+        function form(event){
+            event.preventDefault();
+
+            for(let i = 0; i < input.length; i++)
+            
+            if(input[i].value == ""){
+                alert("You need to type in every input")
+                return false;
+            } else{
+                
+                
+                // Loading data from local storage
+                let data;
+
+                if (localStorage.getItem("data")){
+                    data = JSON.parse(localStorage.getItem("data"))
+                } else {
+                    data = [];
+                }
+
+                // adding the users input to the data array
+                data.push(input[i].value, ticketNumber)
+                // Saving the data in local storage
+                localStorage.setItem("data", JSON.stringify(data));
+
+                
+               
+            }
+            
+            {
+                
+
+                let code = `
+                <div class="card">
+                    <h2 class="heading-card">thanks for purchase, go to your email for more information</h2>
+                    <p class="email-input input">Email : ${email.value}</p>
+                    <p class="ticket-number">Ticket Number : ${ticketNumber}</p>
+                    <p class="ticket-city">Ticket : ${ticket.value}</p>
+                    <div class="btn-card">
+                        <button class="btn-done">done</button>
+                    </div>
+                </div>
+                `
+
+                popupCard.innerHTML = popupCard.innerHTML + code;
+
+                for(let i = 0; i < input.length; i++)
+                
+                input[i].value = "";
+            }
+
+            // Remove class="card"
+            popupCard.addEventListener("click",function(event){
+                if (event.target.classList.contains("btn-done")){
+                    popupCard.removeChild(event.target.parentElement.parentElement)
+                }
+            })
+
+
+
+        }
+        
     }
 }
 
